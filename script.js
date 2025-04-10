517fcf929c817ef5ebbc87876a7c53367160e47e
Vilizar Denichin <denichinv@gmail.com>
2025-04-10 21:10:58 +0100

    add dark mode toggle with img change and white theme icons

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
   menu.classList.toggle('open');
   icon.classList.toggle('open');
 }
+
+function toggleTheme() {
+  const themeIcon = document.getElementById('theme-icon');
+  const profileImg = document.getElementById('prof-pic');
+  const aboutImg = document.getElementById('about-pic');
+  document.body.classList.toggle('dark-mode');
+
+  // Switch icons
+  if (document.body.classList.contains('dark-mode')) {
+    themeIcon.src = 'assets/light.png';
+    profileImg.src = 'assets/profile-pic-dark.jpeg';
+    aboutImg.src = 'assets/about-pic-dark.jpeg';
+  } else {
+    themeIcon.src = 'assets/dark.png';
+    profileImg.src = 'assets/profile-pic.jpg';
+    aboutImg.src = 'assets/about-pic.jpeg';
+  }
+}
