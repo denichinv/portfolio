29d2c1ad48f3f8236091c8cfc8ff366a8667c3e6
Vilizar Denichin <denichinv@gmail.com>
2025-06-06 01:17:28 +0100

    cleaned up unused css and edit typos

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function toggleMenu() {
-  const menu = document.querySelector('.menu-links');
-  const icon = document.querySelector('.hamburger-icon');
-  menu.classList.toggle('open');
-  icon.classList.toggle('open');
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
 function toggleTheme() {
-  const themeIcon = document.getElementById('theme-icon');
-  const profileImg = document.getElementById('prof-pic');
-  const aboutImg = document.getElementById('about-pic');
-  document.body.classList.toggle('dark-mode');
+  const themeIcon = document.getElementById("theme-icon");
+  const profileImg = document.getElementById("prof-pic");
+  const aboutImg = document.getElementById("about-pic");
+  document.body.classList.toggle("dark-mode");
 
   // Switch icons
-  if (document.body.classList.contains('dark-mode')) {
-    themeIcon.src = 'assets/light.png';
-    profileImg.src = 'assets/profile-pic-dark.jpeg';
-    aboutImg.src = 'assets/about-pic-dark.jpeg';
+  if (document.body.classList.contains("dark-mode")) {
+    themeIcon.src = "assets/light.png";
+    profileImg.src = "assets/profile-pic-dark.jpeg";
+    aboutImg.src = "assets/about-pic-dark.jpeg";
   } else {
-    themeIcon.src = 'assets/dark.png';
-    profileImg.src = 'assets/profile-pic.jpg';
-    aboutImg.src = 'assets/about-pic.jpeg';
+    themeIcon.src = "assets/dark.png";
+    profileImg.src = "assets/profile-pic.jpg";
+    aboutImg.src = "assets/about-pic.jpeg";
   }
 }
